f50fd1a1ac08ae0a29353995e86f880d58dd3e75
Gino Grandin <ginograndin@hotmail.com>
2025-11-18 18:22:07 -0800

    Added top stackview for homepage

---
+++ ReclaimLostandFound/HomePageUIView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageUIView.swift
+//  ReclaimLostandFound
+//
+//  Created by csuftitan on 11/18/25.
+//
+
+import SwiftUI
+
+struct HomePageUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageUIView()
+}
