d222d6ed186052bc7891ae511513e83457c24e3f
Gino Grandin <ginograndin@hotmail.com>
2025-11-18 17:57:10 -0800

    Initial Commit

---
+++ ReclaimLostandFound/ReclaimLostandFoundApp.swift
@@ -0,0 +1,21 @@
+//
+//  ReclaimLostandFoundApp.swift
+//  ReclaimLostandFound
+//
+//  Created by csuftitan on 11/18/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct ReclaimLostandFoundApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ ReclaimLostandFoundTests/ReclaimLostandFoundTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReclaimLostandFoundTests.swift
+//  ReclaimLostandFoundTests
+//
+//  Created by csuftitan on 11/18/25.
+//
+
+import Testing
+@testable import ReclaimLostandFound
+
+struct ReclaimLostandFoundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
