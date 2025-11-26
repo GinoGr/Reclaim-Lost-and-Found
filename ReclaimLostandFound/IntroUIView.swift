486ea3384b490592ba074ce5e8b4f1d1d3606828
Gino Grandin <ginograndin@hotmail.com>
2025-11-25 18:04:02 -0800

    Added login/signup page. Beutified it too

---
+++ ReclaimLostandFound/IntroUIView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroUIView.swift
+//  ReclaimLostandFound
+//
+//  Created by csuftitan on 11/25/25.
+//
+
+import SwiftUI
+
+struct IntroUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroUIView()
+}
