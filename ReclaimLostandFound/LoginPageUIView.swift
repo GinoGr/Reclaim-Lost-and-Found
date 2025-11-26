a932d19f71a03b582ecbcbffb9b88b70f0fbca17
Gino Grandin <ginograndin@hotmail.com>
2025-11-25 18:32:46 -0800

    Animated the launch page

---
+++ ReclaimLostandFound/LoginPageUIView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPageUIView.swift
+//  ReclaimLostandFound
+//
+//  Created by csuftitan on 11/25/25.
+//
+
+import SwiftUI
+
+struct LoginPageUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPageUIView()
+}
