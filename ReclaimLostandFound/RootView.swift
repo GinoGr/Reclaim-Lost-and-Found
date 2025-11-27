5ed6e00206cc8e4a0d9eea32bc055c269ddd4153
Gino Grandin <ginograndin@hotmail.com>
2025-11-26 17:53:05 -0800

    Changes root view according to appState

--- ReclaimLostandFound/RootView.swift
+++ ReclaimLostandFound/RootView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct RootView: View {
+    @EnvironmentObject var appState: AppState
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            if appState.isLoggedIn {
+                NavigationStack {
+                    HomePageUIView()
+                }
+            } else {
+                NavigationStack {
+                    IntroUIView()
+                }
+            }
+        }
     }
 }
 
 #Preview {
     RootView()
+        .environmentObject(AppState())
 }
