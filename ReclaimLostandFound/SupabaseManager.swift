8dba33cacbf8fab3d372c8add62efbe3f04437ce
Gino Grandin <38871732+GinoGr@users.noreply.github.com>
2026-02-04 20:03:12 -0800

    Update Supabase key to private placeholder
    Replaced the Supabase key with a placeholder for security.

--- ReclaimLostandFound/SupabaseManager.swift
+++ ReclaimLostandFound/SupabaseManager.swift
@@ -11,7 +11,7 @@ class SupabaseManager {
     // Store url and key for accessing the database. Only accesible by this class
     private init() {
         let url = URL(string: "https://uuxwkzfqbkmqcdvzussh.supabase.co")!
-        let key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1eHdremZxYmttcWNkdnp1c3NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MjAzNDgsImV4cCI6MjA4MDE5NjM0OH0.y0RMrVNlcdV2O6sMNnDfqWhEO_B7JSc3-VVgA2ebuBw"
+        let key = "PRIVATE!!!"
 
         client = SupabaseClient(supabaseURL: url, supabaseKey: key) // initiate the client with the respective database
     }
